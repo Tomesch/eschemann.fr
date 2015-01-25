76f993438846df7d98adf96590d2f9f8ed325391
Thomas Eschemann <thomas@eschemann.fr>
2015-01-25 02:57:54 +0100

    inject css into html to prevent rendering delays, load fonts async

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
 var minifyImages = require('gulp-imagemin');
+var inlinesource = require('gulp-inline-source');
 
 gulp.task('css', function () {
   return gulp.src('./src/css/*.css')
@@ -13,8 +14,9 @@ gulp.task('css', function () {
   .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('html', function() {
+gulp.task('html', ['css'], function() {
   gulp.src('./src/*.html')
+  .pipe(inlinesource({rootpath: './dist'}))
   .pipe(minifyHTML())
   .pipe(gulp.dest('./dist/'))
 });
@@ -30,4 +32,4 @@ gulp.task('cname', function() {
   .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('default', ['css', 'html', 'images', 'cname']);
+gulp.task('default', ['html', 'images', 'cname']);
